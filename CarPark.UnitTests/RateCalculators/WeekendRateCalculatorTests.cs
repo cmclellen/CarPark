c971b74b70e0ebf70f6d30c822fb9f6044311430
cmclellen <cmclellen@gmail.com>
2017-03-21 21:03:22 +1100

    Added tests for WeekendRateCalculator

--- CarPark.UnitTests/RateCalculators/WeekendRateCalculatorTests.cs
+++ CarPark.UnitTests/RateCalculators/WeekendRateCalculatorTests.cs
@@ -1,5 +1,8 @@
 ﻿using CarPark.RateCalculators;
+using CarPark.UnitTests.Helpers;
 using NUnit.Framework;
+using System;
+using System.Linq;
 
 namespace CarPark.UnitTests.RateCalculators
 {
@@ -9,16 +12,87 @@ namespace CarPark.UnitTests.RateCalculators
         [SetUp]
         protected void Setup()
         {
-            SUT = new WeekendRateCalculator();
+            SUT = new WeekendRateCalculator(null);
         }
 
+        private string ExpectedRateName { get; } = "Weekend Rate";
         private WeekendRateCalculator SUT { get; set; }
+        private DateTime inclMinStartDateTime = new DateTime(2017, 3, 25);
+        private DateTime exclMaxEndDateTime = new DateTime(2017, 3, 27);
 
         [Test]
         public void Name_ValidWeekendRateCalculator_CorrectRateName()
         {
             // ASSERT
-            Assert.AreEqual("Weekend Rate", SUT.RateName);
+            Assert.AreEqual(ExpectedRateName, SUT.RateName);
+        }
+
+        [Test]
+        public void Calculate_EligibleStartsAndEndTimes_CorrectPrice()
+        {
+            // ARRANGE
+            var requests =
+                new[]
+                {
+                    new CalculateRequest(inclMinStartDateTime, exclMaxEndDateTime.AddMilliseconds(-1)),
+                };
+
+            // ACT
+            var responses = requests.Select(i => SUT.Calculate(i)).ToList();
+
+            // ASSERT
+            AssertHelpers.AssertCalculateResponses(responses, Enumerable.Repeat(10M, 1).ToList(), ExpectedRateName);
+        }
+
+        [Test]
+        public void Calculate_EligibleStartTimesButIneligibleEndTimes_NotEligible()
+        {
+            // ARRANGE
+            var requests =
+                new[]
+                {
+                    new CalculateRequest(inclMinStartDateTime, exclMaxEndDateTime),
+                };
+
+            // ACT
+            var responses = requests.Select(i => SUT.Calculate(i)).ToList();
+
+            // ASSERT
+            Assert.IsTrue(responses.All(i => i == null));
+        }
+
+        [Test]
+        public void Calculate_IneligibleStartTimesButEligibleEndTimes_NotEligible()
+        {
+            // ARRANGE
+            var requests =
+                new[]
+                {
+                    new CalculateRequest(inclMinStartDateTime.AddMilliseconds(-1), exclMaxEndDateTime.AddMilliseconds(-1)),
+                };
+
+            // ACT
+            var responses = requests.Select(i => SUT.Calculate(i)).ToList();
+
+            // ASSERT
+            Assert.IsTrue(responses.All(i => i == null));
+        }
+
+        [Test]
+        public void Calculate_EligibleStartAndEndTimesOnSameDay_CorrectPrice()
+        {
+            // ARRANGE
+            var requests =
+                new[]
+                {
+                    new CalculateRequest(inclMinStartDateTime, inclMinStartDateTime.AddMinutes(5)),
+                };
+
+            // ACT
+            var responses = requests.Select(i => SUT.Calculate(i)).ToList();
+
+            // ASSERT
+            AssertHelpers.AssertCalculateResponses(responses, Enumerable.Repeat(10M, 1).ToList(), ExpectedRateName);
         }
     }
 }
--- CarPark/RateCalculators/WeekendRateCalculator.cs
+++ CarPark/RateCalculators/WeekendRateCalculator.cs
@@ -1,14 +1,31 @@
-﻿using System;
+﻿using CarPark.Utilities;
+using System;
+using System.Collections.Generic;
 
 namespace CarPark.RateCalculators
 {
-    public class WeekendRateCalculator : IRateCalculator
+    public class WeekendRateCalculator : BaseRateCalculator, IRateCalculator
     {
-        public string RateName { get; } = "Weekend Rate";
-
-        public CalculateResponse Calculate(CalculateRequest request)
+        public WeekendRateCalculator(IRateCalculator successor)
+            : base(successor)
         {
-            throw new NotImplementedException();
+        }
+
+        public override string RateName { get; } = "Weekend Rate";
+
+        public override CalculateResponse Calculate(CalculateRequest request)
+        {
+            Guard.NotNull(() => request, request);
+
+            var startDayOfWeek = request.StartDateTime.DayOfWeek;
+            var endDayOfWeek = request.EndDateTime.DayOfWeek;
+
+            var isEligible = new HashSet<DayOfWeek> { request.StartDateTime.DayOfWeek, request.EndDateTime.DayOfWeek }.IsSubsetOf(new HashSet<DayOfWeek> { DayOfWeek.Saturday, DayOfWeek.Sunday });
+            if (isEligible)
+            {
+                return new CalculateResponse(RateName, 10M);
+            }
+            return base.Calculate(request);
         }
     }
 }
